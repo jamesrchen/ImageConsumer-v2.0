2cc078cfef3352b26f30faf5198bdce01848b698
Asil <misterman348@hotmail.com>
2019-06-29 19:47:02 +0300

    Create ping.js

---
+++ cmd/ping.js
@@ -0,0 +1,8 @@
+module.exports.run = async (client, message, args) => {
+    const msg = await message.channel.send("Ping?");
+    msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
+
+module.exports.help = {
+    name: "ping"
+}
