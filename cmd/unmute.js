dd332ea78fab4abb1f6610bbea063fdea0fbe0dd
Asil <misterman348@hotmail.com>
2019-06-30 01:38:09 +0300

    Added mute/unmute command

---
+++ cmd/unmute.js
@@ -0,0 +1,22 @@
+module.exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You have insufficent permissions to unmute a member!");
+
+    let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!toMute) return message.channel.send("No user specified!");
+
+    if (toMute.id === message.author.id) return message.channel.send("You can't unmute yourself!")
+    if (toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You can't unmute a member who has a higher role than you!");
+
+    let role = message.guild.roles.find(r => r.name === "Muted");
+
+    if (!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not muted!")
+
+    await toMute.removeRole(role);
+    message.channel.send(`${toMute} has been unmuted.`)
+
+    return;
+}
+
+module.exports.help = {
+    name: "unmute"
+}
